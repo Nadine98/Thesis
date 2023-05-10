f7cd00ddc563cf0b5e8d5d48c69f18e216dfc49b
Nadine Kais <67798347+Nadine98@users.noreply.github.com>
2023-05-10 16:24:09 +0200

    Adding the python file for the data processing to the project

---
+++ data_processing.py
@@ -0,0 +1,12 @@
+from os import path 
+
+
+if __name__ =="__main__":
+
+
+    file_format = 'ttl'
+    dataset_path = path.dirname(path.dirname(__file__)) + '/DBpedia 2015-04'
+
+
+    # open the data set folder
+    
